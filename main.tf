f9d2fdd27f006d5e7996c58fb16db768c77deab7
Owoade Taiwo <adexxy@live.com>
2023-09-25 16:50:29 +0100

    provisions vm without with ansible playbook

---
+++ main.tf
@@ -0,0 +1,114 @@
+terraform {
+  required_providers {
+    virtualbox = {
+      source  = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
+    }
+  }
+}
+
+provider "virtualbox" {
+  # Configuration options for the VirtualBox provider (if needed)
+}
+
+resource "virtualbox_vm" "node" {
+  count = var.num_instances
+
+  name      = "${var.instance_name_prefix}-${count.index + 1}"
+  image     = var.image_location
+  cpus      = var.vm_cpus
+  memory    = "2048 MiB"
+    
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "vboxnet1"
+  }
+  
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir -p ~/testfolder",
+      "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config",
+      "sudo systemctl restart sshd",
+      # "sudo mkdir -p ~/.ssh",
+    ]
+
+    connection {
+      host        = self.network_adapter[0].ipv4_address
+      user        = var.user
+      private_key = file(var.vagrant_ssh_key)
+      type        = "ssh"
+      agent       = false
+    }
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      sshpass -p "${var.user_password}" ssh-copy-id -o "StrictHostKeyChecking=no" "${var.user}@${self.network_adapter[0].ipv4_address}"
+    EOT
+
+    connection {
+      host        = self.network_adapter[0].ipv4_address
+      user        = var.user
+      private_key = file(var.vagrant_ssh_key)
+      type        = "ssh"
+      agent       = false
+    }
+  }
+}
+
+# Generate Ansible inventory file content
+locals {
+  ansible_inventory_content = <<-EOT
+    ${join("\n", [for i, vm in virtualbox_vm.node : "${vm.name} ansible_ssh_host=${vm.network_adapter[0].ipv4_address} ip=${vm.network_adapter[0].ipv4_address} flannel_interface=eth1 kube_network_plugin=flannel kube_network_plugin_multus=False download_run_once=True download_localhost=False download_cache_dir=/home/marvel/kubespray_cache download_force_cache=False download_keep_remote_cache=False docker_rpm_keepcache=1 kubeconfig_localhost=True kubectl_localhost=True local_path_provisioner_enabled=False local_path_provisioner_claim_root=/opt/ ansible_ssh_user='vagrant' ansible_ssh_private_key_file='${var.vagrant_ssh_key}' "])}
+
+    [etcd]
+    kub-[1:3]
+
+    [kube_control_plane]
+    kub-[1:2]
+
+    [kube-node]
+    kub-[1:3]
+
+    [k8s-cluster:children]
+    kube-master
+    kube-node
+
+  EOT
+}
+
+# Create Ansible inventory file
+resource "local_file" "inventory_file" {
+  filename = "/home/marvel/Documents/DevOps/project-terraform/terraform-virtualbox-kubespray/kubespray/inventory.ini"
+  content  = local.ansible_inventory_content
+}
+
+# Run Ansible playbook
+resource "null_resource" "run_ansible" {
+  triggers = {
+    # Map the server names to their corresponding IDs
+    for i, vm in virtualbox_vm.node : i => vm.id
+  }
+
+  provisioner "local-exec" {
+    command     = "ansible-playbook -i /home/marvel/Documents/DevOps/project-terraform/terraform-virtualbox-kubespray/kubespray/inventory.ini -u ${var.user} -e 'variable=value' ${var.playbook} -b -vvv --private-key=/home/marvel/.vagrant.d/insecure_private_key"
+    working_dir = path.module
+
+    connection {
+      host        = element([for vm in virtualbox_vm.node : vm.network_adapter[0].ipv4_address], 0) # Access the first IP address
+      user        = var.user
+      private_key = file(var.vagrant_ssh_key)
+      type        = "ssh"
+      agent       = false
+    }
+  }
+
+  depends_on = [local_file.inventory_file]
+  
+  
+}
+
+output "IPAddresses" {
+  value = [for vm in virtualbox_vm.node : vm.network_adapter[0].ipv4_address]
+}
---
+++ variables.tf
@@ -0,0 +1,159 @@
+variable "server_names" {
+  type    = list(string)
+  default = ["master1","master2","worker"]
+}
+
+variable "public_key_location" {
+  description = "public key location"
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "ssh_key_private" {
+  description = "private key location"
+  default = "~/.ssh/id_rsa"
+}
+
+variable "image_location" {
+  description = "virtualbox image location - can be downloaded or remote location"
+  default = "/bionic-server-cloudimg-amd64-vagrant.box"
+}
+
+variable "vagrant_ssh_key" {
+  description = "default vagrant ssh key location - not secure"
+  default = "~/.vagrant.d/insecure_private_key"
+}
+
+variable "user" {
+  description = "user to use the vm"
+  default = "vagrant"
+}
+
+variable "user_password" {
+  description = "user password to use the vm"
+  default = "vagrant"
+}
+
+# variable "network_adapter_type" {
+#   description = "network adapter type, others are NAT, NAT network, Bridged, internal network etc"
+#   default = "bridged"
+# }
+
+variable "hostonly_ipv4_subnet" {
+  description = "The private IPv4 subnet of the VirtualBox hostonly network (default is 192.168.56.0/16)"
+  type        = string
+  default     = "192.168.59.0/16"
+
+  validation {
+    condition     = can(cidrnetmask(var.hostonly_ipv4_subnet)) && split("/", var.hostonly_ipv4_subnet)[1] == "16"
+    error_message = "The specified hostonly IPv4 CIDR is invalid."
+  }
+}
+
+variable "internet_gateway" {
+  description = "internet network gateway"
+  default = "nat"
+}
+
+### varaition added from here
+
+variable "instance_name_prefix" {
+  description = "Prefix for instance names"
+  type        = string
+  default     = "kub"
+}
+
+variable "vm_cpus" {
+  description = "Number of CPUs for VM instances"
+  type        = number
+  default     = 2
+}
+
+variable "num_instances" {
+  description = "Number of VM instances to create"
+  type        = number
+  default     = 3
+}
+
+variable "etcd_instances" {
+  description = "Number of etcd instances"
+  type        = number
+  default     = 3
+}
+
+variable "kube_master_instances" {
+  description = "Number of kube master instances"
+  type        = number
+  default     = 2
+}
+
+variable "kube_node_instances" {
+  description = "Number of kube node instances"
+  type        = number
+  default     = 3
+}
+
+variable "kube_node_instances_with_disks" {
+  description = "Whether to create kube node instances with disks"
+  type        = bool
+  default     = false
+}
+
+variable "kube_node_instances_with_disks_size" {
+  description = "Size of kube node instance disks"
+  type        = string
+  default     = "20G"
+}
+
+variable "kube_node_instances_with_disks_number" {
+  description = "Number of kube node instance disks"
+  type        = number
+  default     = 2
+}
+
+variable "override_disk_size" {
+  description = "Whether to override disk size"
+  type        = bool
+  default     = false
+}
+
+variable "disk_size" {
+  description = "Disk size for VM instances"
+  type        = string
+  default     = "20GB"
+}
+
+variable "local_path_provisioner_enabled" {
+  description = "Enable local path provisioner"
+  type        = bool
+  default     = false
+}
+
+variable "local_path_provisioner_claim_root" {
+  description = "Local path provisioner claim root directory"
+  type        = string
+  default     = "/opt/local-path-provisioner/"
+}
+
+variable "libvirt_nested" {
+  description = "Enable libvirt nested virtualization"
+  type        = bool
+  default     = false
+}
+
+variable "ansible_verbosity" {
+  description = "Ansible verbosity level"
+  type        = string
+  default     = false
+}
+
+variable "ansible_tags" {
+  description = "Ansible tags"
+  type        = string
+  default     = ""
+}
+
+variable "playbook" {
+  description = "Name of the Ansible playbook to execute"
+  type        = string
+  default     = "cluster.yml"
+}
